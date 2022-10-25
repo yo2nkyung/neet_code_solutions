ee5e870727f6caf838563035d55e7f293d464e91
YOONKYUNG LEE <yklee.tess@gmail.com>
2022-10-25 17:40:45 +0900

    Time: 47 ms, Memory: 13.9 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def duplicateZeros(self, arr):
+        arr_tmp = []
+        
+        for num in arr :
+            if num == 0 :
+                arr_tmp.append(num)
+            arr_tmp.append(num)
+        
+        result = arr_tmp[:len(arr)]
+        
+        for i in range(len(arr)) :
+            arr[i] = arr_tmp[i]
+        
+        """
+        :type arr: List[int]
+        :rtype: None Do not return anything, modify arr in-place instead.
+        """
+        

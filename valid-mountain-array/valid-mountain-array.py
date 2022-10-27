ef72b5e0f1ef0a68db7de6ba09709f97575fc3fe
YOONKYUNG LEE <yklee.tess@gmail.com>
2022-10-27 18:24:49 +0900

    Time: 442 ms, Memory: 14.9 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def validMountainArray(self, arr):
+        if len(arr) < 3:
+            return False
+        if arr[0] > arr[1] :
+            return False
+        if arr[len(arr) - 1] > arr[len(arr) - 2]:
+            return False
+        
+        for i in range(len(arr) - 1):
+            if arr[i] == arr[i+1]:
+                return False
+        
+        for i in range(len(arr) - 2):
+            if arr[i] > arr[i+1] and arr[i+1] < arr[i+2]:
+                return False
+            
+        
+            
+        
+        return True
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        

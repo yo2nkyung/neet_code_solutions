1846dc6e102376a367a73443eabddd6378a2c690
YOONKYUNG LEE <yklee.tess@gmail.com>
2022-10-27 18:25:59 +0900

    Time: 164 ms, Memory: 14.6 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.py
+++ valid-mountain-array/valid-mountain-array.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def validMountainArray(self, arr):
-        if len(arr) < 3 or arr[0] > arr[1] or arr[len(arr) - 1] > arr[len(arr) - 2]:
+        if (len(arr) < 3) or (arr[0] > arr[1]) or (arr[len(arr) - 1] > arr[len(arr) - 2]):
             return False
         
         for i in range(len(arr) - 1):

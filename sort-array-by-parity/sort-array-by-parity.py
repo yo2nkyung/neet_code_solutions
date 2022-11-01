81a0ea6fdded218c443819ed60e401df5c9fe479
YOONKYUNG LEE <yklee.tess@gmail.com>
2022-11-01 15:59:19 +0900

    Time: 61 ms, Memory: 14.2 MB - LeetHub

--- sort-array-by-parity/sort-array-by-parity.py
+++ sort-array-by-parity/sort-array-by-parity.py
@@ -2,8 +2,9 @@ class Solution(object):
     def sortArrayByParity(self, nums):
         left = 0
         right = len(nums) - 1
+        
         while left < right:
-            if nums[left] % 2:
+            if nums[left] % 2:              #숫자 1은 True, 숫자 0은 False
                 nums[left], nums[right] = nums[right], nums[left]
                 right -= 1
             else:

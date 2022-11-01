aa86e7bfcaa983c55e5fb84e68bd73d529b19864
YOONKYUNG LEE <yklee.tess@gmail.com>
2022-11-01 17:14:33 +0900

    Time: 83 ms, Memory: 14.3 MB - LeetHub

---
+++ third-maximum-number/third-maximum-number.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        
+        expected = sorted(nums, reverse=True)
+        largest = expected[0]
+        
+        if len(expected) < 3:
+            return largest
+        count = 0
+        
+        for i in range(1, len(expected)):
+
+            if expected[i] < largest:
+                largest = expected[i]
+                count += 1
+            if count == 2:
+                break
+        
+        if count < 2:
+            largest = expected[0]
+        
+        return largest
+                
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        

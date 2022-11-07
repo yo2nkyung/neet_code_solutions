f24e126aec1cf98f146c1c5f3e47b5a75ca43344
YOONKYUNG LEE <yklee.tess@gmail.com>
2022-11-07 16:55:58 +0900

    Time: 278 ms, Memory: 24.9 MB - LeetHub

---
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        result = set(range(1, len(nums)+1))
+        
+        nums = set(nums)
+        
+        result = result - nums
+        #포문을 2 개 돌리니 시간 초과. 포문을 사용하지 않는 방향으로 숮2ㅓㅇ
+        #파이썬 자료형 set을 사용
+        return result
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
